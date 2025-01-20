ba2aca3237e31952bfcb412e649123cf1b2e7f1b
Shreya <shreya16180@gmail.com>
2025-01-20 17:05:26 +0530

    adding MAP()

--- calc-1/src/components/ButtonContainer.jsx
+++ calc-1/src/components/ButtonContainer.jsx
@@ -1,13 +1,40 @@
 import styles from "./ButtonContainer.module.css";
 
 const ButtonContainer = () => {
+	const buttonNames = [
+		"C",
+		"1",
+		"2",
+		"+",
+		"3",
+		"4",
+		"-",
+		"5",
+		"6",
+		"+",
+		"7",
+		"8",
+		"/",
+		"9",
+		"=",
+		"0",
+		".",
+	];
+
 	return (
 		<>
+			{/* use map method */}
+
 			<div className={styles.buttonsContainer}>
+				{buttonNames.map((buttonNames) => (
+					<button className={styles.button}>{buttonNames}</button>
+				))}
+
+				{/* REPLACED with map()
 				<button className={styles.button}>C</button>
 				<button className={styles.button}>C</button>
 				<button className={styles.button}>C</button>
-				<button className={styles.button}>C</button>
+				<button className={styles.button}>C</button> */}
 			</div>
 		</>
 	);
